a78ee959b0448bb9ac794b1edc0235d10f8d5e2b
FARHAAN-M <farhaanrahman798@gmail.com>
2026-07-16 21:03:52 -0400

    Add files via upload

---
+++ DataTypeAndVariable.java
@@ -0,0 +1,15 @@
+class DataTypeAndVariable{
+    public static void main(String a[])
+    {
+        String name="FARHAAN RAHMAN";
+        int age=18;
+        int phone_no=1234567890;
+        String father_name="MUJIPUR RAHMAN";
+        String mother_name="FATHIMA BI";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(phone_no);
+        System.out.println(father_name);
+        System.out.println(mother_name);
+    }
+}
